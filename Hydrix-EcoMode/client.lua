214ce520f06f541c3a09be7f471288acc141e33c
/Hydrix_Scripts <84548316+imtorchedbud@users.noreply.github.com>
2023-04-21 13:23:28 -0400

    Update client.lua

--- Hydrix-EcoMode/client.lua
+++ Hydrix-EcoMode/client.lua
@@ -1,65 +1,47 @@
 -- Load the QB-Core module
 local qbcore = exports["qb-core"]
 
--- Define the available engine sounds
-local engineSounds = {
-  ["ENGINE_LOWRIDER"] = true,
-  ["ENGINE_GENERIC"] = true,
-  ["ENGINE_SUPERCAR"] = true,
-  ["ENGINE_SPORT"] = true,
-}
-
--- Function to play a specific engine sound for a vehicle
-function playEngineSound(vehicle, soundName)
-  -- Use the '_FORCE_VEHICLE_ENGINE_AUDIO' native provided by cfx docs
-  local isSuccessful = Citizen.invokeNative("0x1F1F957154EC51DF", vehicle, soundName)
-  return isSuccessful
+-- Event handler for when the server requests the engine sound to be changed
+local function onChangeSound(soundName)
+  -- Play the new engine sound
+  TriggerEvent("qb-enginesound:playEngineSound", soundName)
 end
 
--- Function to handle the change sound request
-local changeSoundFunction = Instance.new("RemoteFunction")
-changeSoundFunction.Name = "ChangeSound"
-changeSoundFunction.OnServerInvoke = function(player, soundName)
-  if engineSounds[soundName] then
-    local vehicle = player.Character and player.Character:FindFirstChildOfClass("Vehicle")
-    if vehicle then
-      -- Play the engine sound that was set by the user
-      local isSuccessful = playEngineSound(vehicle, soundName)
-      if isSuccessful then
-        return true
-      end
-    end
-  end
-  return false
-end
-changeSoundFunction.Parent = game:GetService("ReplicatedStorage")
-
--- Function to handle the keybind event
-function handleEngineSoundChange()
-  -- Get the player who triggered the event
-  local player = source
-
-  local vehicle = player.Character and player.Character:FindFirstChildOfClass("Vehicle")
-  if vehicle then
-    -- Show the menu to choose the new engine sound
-    qbcore:OpenMenu("default", function(data)
-      local soundName = data.current.value
-      -- Send the request to the server to change the engine sound
-      local isSuccessful = changeSoundFunction:InvokeClient(player, soundName)
-      if isSuccessful then
-        createProgressBar(player, "Changing engine sound", 2500)
-      end
-    end, { header = "Change Engine Sound", align = "bottom-right", elements = {
-      { label = "Engine Lowrider", value = "ENGINE_LOWRIDER" },
-      { label = "Engine Generic", value = "ENGINE_GENERIC" },
-      { label = "Engine Supercar", value = "ENGINE_SUPERCAR" },
-      { label = "Engine Sport", value = "ENGINE_SPORT" },
-    }})
-  end
+-- Function to handle the engine sound menu
+function handleEngineSoundMenu()
+  -- Show the menu to choose the new engine sound
+  qbcore:OpenMenu("default", function(data)
+    local soundName = data.current.value
+    -- Send the request to the server to change the engine sound
+    TriggerServerEvent("qb-enginesound:changeSound", soundName)
+    createProgressBar("Changing engine sound", 2500)
+  end, { header = "Change Engine Sound", align = "bottom-right", elements = {
+    { label = "Engine Lowrider", value = "ENGINE_LOWRIDER" },
+    { label = "Engine Generic", value = "ENGINE_GENERIC" },
+    { label = "Engine Supercar", value = "ENGINE_SUPERCAR" },
+    { label = "Engine Sport", value = "ENGINE_SPORT" },
+  }})
 end
 
--- Register the 'k' keybind to change the engine sound
+-- Register the 'k' keybind to show the engine sound menu
 RegisterKeyMapping("k", "Change Engine Sound", "keyboard", "k")
 RegisterCommand("changesound", function()
-  handleEngineSoundChange()
-end, false
+  handleEngineSoundMenu()
+end, false)
+
+-- Play the specified engine sound for a vehicle
+AddEventHandler("qb-enginesound:playEngineSound", function(soundName)
+  -- Use the '_FORCE_VEHICLE_ENGINE_AUDIO' native provided by cfx docs
+  local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+  if DoesEntityExist(vehicle) then
+    Citizen.InvokeNative("0x1F1F957154EC51DF", vehicle, soundName)
+  end
+end)
+
+-- Event handler for when the player enters a vehicle
+AddEventHandler("playerEnteredVehicle", function(vehicle)
+  -- Get the current engine sound for the vehicle
+  local soundName = GetVehicleMod(vehicle, 11)
+  -- Play the current engine sound
+  TriggerEvent("qb-enginesound:playEngineSound", soundName)
+end)
