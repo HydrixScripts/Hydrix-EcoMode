7f495487b07ff2cfdd671aa28ce459d16323ef51
/Hydrix_Scripts <84548316+imtorchedbud@users.noreply.github.com>
2023-04-21 13:24:31 -0400

    Create config.lua

---
+++ Hydrix-EcoMode/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+-- The available engine sounds
+Config.EngineSounds = {
+    ["ENGINE_LOWRIDER"] = "Engine Lowrider",
+    ["ENGINE_GENERIC"] = "Engine Generic",
+    ["ENGINE_SUPERCAR"] = "Engine Supercar",
+    ["ENGINE_SPORT"] = "Engine Sport",
+}
+
+-- The key binding to trigger the engine sound change
+Config.KeyBinding = "k"
+
+-- The duration of the progress bar shown when changing engine sound
+Config.ProgressBarDuration = 2500
